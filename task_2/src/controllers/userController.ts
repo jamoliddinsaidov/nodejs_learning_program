308d5a57b0531a3bd9f8b411f9eaa0755a853952
Jamoliddin Saidov <sjamoliddin21@gmail.com>
2023-02-09 21:26:06 +0500

    use UserService in UserController

--- task_2/src/controllers/userController.ts
+++ task_2/src/controllers/userController.ts
@@ -1,193 +1,96 @@
 import { Request, Response } from 'express'
-import fs from 'fs/promises'
 import { ValidationError } from 'joi'
-import { v4 as uuidv4 } from 'uuid'
-import { User, UserRequestBody, userRequestSchema } from '../models/User.js'
-import {
-  USER_CREATED_MESSAGE,
-  USER_UPDATED_MESSAGE,
-  USER_DELETED_MESSAGE,
-  SOMETHING_WENT_WRONG_MESSAGE,
-  NO_USER_FOUND_MESSAGE,
-  NO_LOGIN_SUBSTRING_MESSAGE,
-  NO_USERS_FOUND_MATCHING_LOGIN_SUBSTRING_MESSAGE,
-  LOGIN_NOT_AVAILABLE_MESSAGE,
-  databaseFilePath,
-} from './constants.js'
-import { jsonStringfy } from '../utils/jsonStringfy.js'
-import { sortUserByLogin } from '../utils/sortUserByLogin.js'
+import { UserRequestBody, userRequestSchema } from '../models/schema/userSchema.js'
+import { getGenericErrorMessage } from '../utils/index.js'
+import { UserService } from '../services/User.js'
+import { IUser } from '../models/User.js'
+
+const userService = new UserService()
 
 export const getUserById = async (req: Request, res: Response) => {
   try {
-    const userId = req.params.id
-    const user = await _getUserById(userId)
+    const userId = parseInt(req.params.id)
+    const result = await userService.getById(userId)
 
-    if (!user) {
-      return res.status(400).json({
-        success: false,
-        message: NO_USER_FOUND_MESSAGE,
-        error: userId,
-      })
+    if (result?.error) {
+      return res.status(400).json(result.error)
     }
 
-    res.status(200).json({
-      success: true,
-      data: user,
-    })
+    res.status(200).json(result.response)
   } catch (error) {
-    res.status(500).json({
-      success: false,
-      message: SOMETHING_WENT_WRONG_MESSAGE,
-      error: error.message,
-    })
+    res.status(500).json(getGenericErrorMessage(error))
   }
 }
 
 export const createUser = async (req: Request, res: Response) => {
-  const { error, value }: { error: ValidationError; value: UserRequestBody } = userRequestSchema.validate(req.body)
+  const schemaValidation: { error: ValidationError; value: UserRequestBody } = userRequestSchema.validate(req.body)
 
-  if (error) {
+  if (schemaValidation.error) {
     return res.status(400).json({
       success: false,
-      message: error.details[0].message,
-      error,
+      message: schemaValidation.error.details[0].message,
+      error: schemaValidation.error,
     })
   }
 
   try {
-    const users = await _getUsers()
-    const isLoginNotAvailable = users.find((user) => user.login === value.login)
+    const result = await userService.create(schemaValidation.value)
 
-    if (isLoginNotAvailable) {
-      return res.status(400).json({
-        success: false,
-        message: LOGIN_NOT_AVAILABLE_MESSAGE,
-        data: value,
-      })
+    if (result?.error) {
+      return res.status(400).json(result.error)
     }
 
-    const user: User = {
-      id: uuidv4(),
-      login: value.login,
-      password: value.password,
-      age: value.age,
-      isDeleted: false,
-    }
-
-    const newUsers = jsonStringfy([...users, user])
-
-    await fs.writeFile(databaseFilePath, newUsers)
-
-    res.status(201).json({
-      success: true,
-      message: USER_CREATED_MESSAGE,
-      data: user,
-    })
+    res.status(201).json(result.response)
   } catch (error) {
-    res.status(500).json({
-      success: false,
-      message: SOMETHING_WENT_WRONG_MESSAGE,
-      error: error.message,
-    })
+    res.status(500).json(getGenericErrorMessage(error))
   }
 }
 
 export const updateUser = async (req: Request, res: Response) => {
   try {
-    // check if the user with the provided ID exists
-    const userId = req.params.id
-    const user = await _getUserById(userId)
+    const schemaValidation: { error: ValidationError; value: UserRequestBody } = userRequestSchema.validate(req.body)
 
-    if (!user) {
-      return res.status(404).json({
-        success: false,
-        error: NO_USER_FOUND_MESSAGE,
-        data: userId,
-      })
-    }
-
-    // check if all the required fields are provided
-    const { error, value }: { error: ValidationError; value: UserRequestBody } = userRequestSchema.validate(req.body)
-
-    if (error) {
+    if (schemaValidation.error) {
       return res.status(400).json({
         success: false,
-        message: error.details[0].message,
-        error,
+        message: schemaValidation.error.details[0].message,
+        error: schemaValidation.error,
       })
     }
 
-    const users = await _getUsers()
-    const isLoginNotAvailable = users.find((user) => user.login === value.login)
-
-    if (isLoginNotAvailable) {
-      return res.status(400).json({
-        success: false,
-        message: LOGIN_NOT_AVAILABLE_MESSAGE,
-        data: value,
-      })
+    const userId = parseInt(req.params.id)
+    const updatedUser: IUser = {
+      id: userId,
+      login: schemaValidation.value.login,
+      password: schemaValidation.value.password,
+      age: schemaValidation.value.age,
+      is_deleted: false,
     }
 
-    const updatedUser: User = {
-      id: user.id,
-      login: value.login,
-      password: value.password,
-      age: value.age,
-      isDeleted: user.isDeleted,
+    const result = await userService.update(updatedUser)
+
+    if (result?.error) {
+      return res.status(400).json(result.error)
     }
 
-    const updatedUserIndex = users.findIndex((user) => user.id === updatedUser.id)
-    users[updatedUserIndex] = updatedUser
-
-    await fs.writeFile(databaseFilePath, jsonStringfy(users))
-
-    res.status(200).json({
-      success: true,
-      message: USER_UPDATED_MESSAGE,
-      data: updatedUser,
-    })
+    res.status(200).json(result.response)
   } catch (error) {
-    res.status(500).json({
-      success: false,
-      message: SOMETHING_WENT_WRONG_MESSAGE,
-      error: error.message,
-    })
+    res.status(500).json(getGenericErrorMessage(error))
   }
 }
 
 export const deleteUser = async (req: Request, res: Response) => {
   try {
-    // check if the user with the provided ID exists
-    const userId = req.params.id
-    const user = await _getUserById(userId)
+    const userId = parseInt(req.params.id)
+    const result = await userService.delete(userId)
 
-    if (!user) {
-      return res.status(400).json({
-        success: false,
-        error: NO_USER_FOUND_MESSAGE,
-        data: userId,
-      })
+    if (result?.error) {
+      return res.status(400).json(result.error)
     }
 
-    const users = await _getUsers()
-    const deletedUser: User = { ...user, isDeleted: true }
-
-    const deletedUserIndex = users.findIndex((user) => user.id === deletedUser.id)
-    users[deletedUserIndex] = deletedUser
-
-    await fs.writeFile(databaseFilePath, jsonStringfy(users))
-
-    res.status(200).json({
-      success: true,
-      message: USER_DELETED_MESSAGE,
-      data: deletedUser,
-    })
+    res.status(200).json(result.response)
   } catch (error) {
-    res.status(500).json({
-      success: false,
-      message: SOMETHING_WENT_WRONG_MESSAGE,
-      error: error.message,
-    })
+    res.status(500).json(getGenericErrorMessage(error))
   }
 }
 
@@ -195,52 +98,14 @@ export const getAutoSuggestUsers = async (req: Request, res: Response) => {
   try {
     const { loginSubstring, limit = 10 } = req.body
 
-    if (!loginSubstring) {
-      return res.status(400).json({
-        success: false,
-        error: NO_LOGIN_SUBSTRING_MESSAGE,
-      })
+    const result = await userService.autoSuggest(loginSubstring, limit)
+
+    if (result?.error) {
+      return res.status(400).json(result.error)
     }
 
-    const searchRegex = new RegExp(loginSubstring, 'gi')
-
-    const users = await _getUsers()
-    const suggestedUsers = users
-      .filter((user) => user.login.match(searchRegex))
-      .sort((userA, userB) => sortUserByLogin(userA, userB, loginSubstring))
-
-    if (!suggestedUsers.length) {
-      return res.status(404).json({
-        success: false,
-        error: NO_USERS_FOUND_MATCHING_LOGIN_SUBSTRING_MESSAGE,
-        data: loginSubstring,
-      })
-    }
-
-    if (suggestedUsers.length > limit) {
-      suggestedUsers.length = limit
-    }
-
-    res.status(200).json({
-      success: true,
-      data: suggestedUsers,
-    })
+    res.status(200).json(result.response)
   } catch (error) {
-    res.status(500).json({
-      success: false,
-      message: SOMETHING_WENT_WRONG_MESSAGE,
-      error: error.message,
-    })
+    res.status(500).json(getGenericErrorMessage(error))
   }
-}
-
-const _getUsers = async () => {
-  const data = await fs.readFile(databaseFilePath, 'utf-8')
-  return JSON.parse(data) as User[]
-}
-
-const _getUserById = async (userId: string) => {
-  const users = await _getUsers()
-
-  return users.find((user) => user.id === userId)
 }
--- task_2/src/utils/jsonStringfy.ts
+++
@@ -1,5 +0,0 @@
-import { User } from '../models/schema/userSchema.js'
-
-export const jsonStringfy = (users: User[]) => {
-  return JSON.stringify(users, null, 2)
-}
