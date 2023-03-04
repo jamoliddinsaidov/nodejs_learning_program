e1b10f5f11024d7e3bddb4cf355d881797cd5da9
Jamoliddin Saidov <sjamoliddin21@gmail.com>
2023-03-04 16:13:59 +0500

    feat: add credentials and verifyJWT middlewares

--- task_2/src/middlewares/index.ts
+++ task_2/src/middlewares/index.ts
@@ -1,4 +1,6 @@
 import { logger } from './logger.js'
 import { logUnhandledError } from './logUnhandledError.js'
+import { verifyJWT } from './verifyJWT.js'
+import { addCredentialsHeader } from './credentials.js'
 
-export { logger, logUnhandledError }
+export { logger, logUnhandledError, verifyJWT, addCredentialsHeader }
---
+++ task_2/src/middlewares/verifyJWT.ts
@@ -0,0 +1,22 @@
+import { Request, Response, NextFunction } from 'express'
+import jwt from 'jsonwebtoken'
+import dotenv from 'dotenv'
+import { AUTHORIZATION_HEADER_NOT_PROVIDED, AUTHORIZATION_TOKEN_INVALID } from '../services/constants.js'
+dotenv.config()
+
+export const verifyJWT = (req: Request, res: Response, next: NextFunction) => {
+  const authHeader = req.headers['authorization']
+
+  if (!authHeader) {
+    return res.status(401).json({ success: false, message: AUTHORIZATION_HEADER_NOT_PROVIDED })
+  }
+
+  const [type, token] = authHeader.split(' ')
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (error, decoded) => {
+    if (error) {
+      return res.status(403).json({ success: false, message: AUTHORIZATION_TOKEN_INVALID, error })
+    }
+
+    next()
+  })
+}
